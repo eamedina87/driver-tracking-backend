7db1f03dc722a9fd6c2b0e5b34f1ed1d1ea98cc3
Erick Medina <eamedina@protonmail.com>
2021-03-21 16:29:35 +0100

    change structure, change customer to customer_name

--- deliveries.py
+++ deliveries.py
@@ -7,16 +7,21 @@ base_lat = 41.39271869720619
 def create_deliveries():
     print("creating_deliveries...")
     deliveries = []
-    for index in range(1, 51):
+    for index in range(1, 31):
         delivery = {
             "id": index,
             "address": "Address {}".format(index),
             "latitude": base_lat + index * 0.0001,
             "longitude": base_long + index * 0.001,
-            "customer": "Customer {}".format(index)
+            "customer_name": "Customer {}".format(index)
         }
         deliveries.append(delivery)
-    print(json.dumps(deliveries))
+    obj = {
+        "deliveries": {
+            "deliveries": deliveries
+        }
+    }
+    print(json.dumps(obj))
 
 
 if __name__ == "__main__":
